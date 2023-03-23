4cde395844094a4b1d125d75802d8e7f65d60dc3
sunilShetty <sunilShetty@gmail.com>
2023-03-23 13:59:06 +0530

    User of Y code commited

--- src/test/java/org/asurion/TestDSAKnowledge.java
+++ src/test/java/org/asurion/TestDSAKnowledge.java
@@ -8,10 +8,13 @@ public class TestDSAKnowledge {
         int[] arr = new int[5];
         arr[0] = 2;
         arr[1] = 4;
-        (int i: arr)
+
         System.out.println(arr);
         int[] a2 = {1,2,3,4,5};
         System.out.println(a2[3]);
+        for(int i : a2){
+            System.out.println(i);
+        }
 
     }
 
