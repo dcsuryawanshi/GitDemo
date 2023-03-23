090a787e60d01934b31860ed6e5f5dc3d8fb9c7e
sunilShetty <sunilShetty@gmail.com>
2023-03-23 19:12:42 +0530

    TestDSAKnowledge change

--- src/test/java/org/asurion/TestDSAKnowledge.java
+++ src/test/java/org/asurion/TestDSAKnowledge.java
@@ -2,6 +2,11 @@ package org.asurion;
 
 import com.google.gson.internal.bind.util.ISO8601Utils;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TestDSAKnowledge {
 
     public static void main(String[] args){
@@ -9,12 +14,21 @@ public class TestDSAKnowledge {
         arr[0] = 2;
         arr[1] = 4;
 
+        String[] array = {"apple", "banana", "orange"};
+        List<String> list = new ArrayList<>(Arrays.asList(array));
+
+        Integer[] iArray = {1,2,3,4,5};
+        List<Integer> iArrayList = new ArrayList<Integer>(Arrays.asList(iArray));
+
         System.out.println(arr);
         int[] a2 = {1,2,3,4,5};
         System.out.println(a2[3]);
         for(int i : a2){
             System.out.println(i);
         }
+        Integer[] integerArray = Arrays.stream(a2).boxed().toArray(Integer[]::new);
+        List<Integer> arrList = new ArrayList<Integer>(List.of(integerArray));
+
 
     }
 
