693b0aeb99926d2ea70812a2a773bc8117fd1378
dcsuryawanshi <dcsuryawanshi@gmail.com>
2023-03-24 18:04:37 +0530

    X added new test

--- src/test/java/org/asurion/FlightTest.java
+++ src/test/java/org/asurion/FlightTest.java
@@ -82,6 +82,38 @@ public class FlightTest {
         thp.setBookingStrategy(new RoundTrip());
         thp.checkAvailability("Origin", "destination");
     }
+
+    @Test
+    public void getHeaderFlightDetails4(){
+//        TravelHomePage thp = new TravelHomePage(driver);
+//        thp.gotoHome();
+        String flightClass = thp.getNavigationBar().getFlightsClassName();
+        System.out.println(flightClass);
+        Assert.assertEquals(flightClass, "selected");
+        System.out.println(thp.getNavigationBar().getLinkCount());
+
+        thp.setBookingStrategy(new MultiTrip());
+        thp.checkAvailability("Origin", "destination");
+
+        thp.setBookingStrategy(new RoundTrip());
+        thp.checkAvailability("Origin", "destination");
+    }
+
+    @Test
+    public void getHeaderFlightDetails5(){
+//        TravelHomePage thp = new TravelHomePage(driver);
+//        thp.gotoHome();
+        String flightClass = thp.getNavigationBar().getFlightsClassName();
+        System.out.println(flightClass);
+        Assert.assertEquals(flightClass, "selected");
+        System.out.println(thp.getNavigationBar().getLinkCount());
+
+        thp.setBookingStrategy(new MultiTrip());
+        thp.checkAvailability("Origin", "destination");
+
+        thp.setBookingStrategy(new RoundTrip());
+        thp.checkAvailability("Origin", "destination");
+    }
     @AfterTest
     public void closeBrowser(){
         driver.quit();
